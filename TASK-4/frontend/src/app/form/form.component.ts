b1279554e30f7d2ccf443f71a5a24d0eb5482fbf
rudrapatirohith <rudrapatirohith1@gmail.com>
2024-08-20 22:08:20 -0500

    linked signup,login pages with backend and worked on  authentications for forms page

--- TASK-4/frontend/src/app/form/form.component.ts
+++ TASK-4/frontend/src/app/form/form.component.ts
@@ -63,54 +63,51 @@ export class FormComponent {
   }
 
 
-  // Handles the checkbox change event in my form
-  onCheckboxChange(e: any) {
-    // I'm grabbing the 'checkbox_list' FormArray from my form
-    const checkboxArray: FormArray = this.insertForm.get('checkbox_list') as FormArray;
 
-    // If the checkbox is checked
-    if (e.target.checked) {
-      // pushing the checkbox value into the FormArray as a new FormControl
-      checkboxArray.push(new FormControl(e.target.value));
+  onCheckboxChange(e: any) {   // Handles the checkbox change event in my form
+ 
+    const checkboxArray: FormArray = this.insertForm.get('checkbox_list') as FormArray;    // I'm grabbing the 'checkbox_list' FormArray from my form
+
+    
+    if (e.target.checked) {// If the checkbox is checked
+      
+      checkboxArray.push(new FormControl(e.target.value)); // pushing the checkbox value into the FormArray as a new FormControl
     } else {
-      // If the checkbox is unchecked, finding the index of the FormControl that matches the value
-      const index = checkboxArray.controls.findIndex(x => x.value === e.target.value);
+      
+      const index = checkboxArray.controls.findIndex(x => x.value === e.target.value); // If the checkbox is unchecked, finding the index of the FormControl that matches the value
 
-      // removing that FormControl from the FormArray
-      checkboxArray.removeAt(index);
+     
+      checkboxArray.removeAt(index);  // removing that FormControl from the FormArray
     }
   }
 
-  // Handles the list box change event in my form
-  onListBoxChange(e: any) {
-    // I'm grabbing the 'list_box' FormArray from my form
-    const listBoxArray: FormArray = this.insertForm.get('list_box') as FormArray;
 
-    // If an option in the list box is selected
-    if (e.target.selected) {
-      // pushing the selected value into the FormArray as a new FormControl
-      listBoxArray.push(new FormControl(e.target.value));
+  onListBoxChange(e: any) {   // Handles the list box change event in my form
+    
+    const listBoxArray: FormArray = this.insertForm.get('list_box') as FormArray; // I'm grabbing the 'list_box' FormArray from my form
+
+    if (e.target.selected) {     // If an option in the list box is selected
+    
+      listBoxArray.push(new FormControl(e.target.value));   // pushing the selected value into the FormArray as a new FormControl
     } else {
-      // If the option is deselected, it will find the index of the FormControl with that value
-      const index = listBoxArray.controls.findIndex(x => x.value === e.target.value);
+      
+      const index = listBoxArray.controls.findIndex(x => x.value === e.target.value); // If the option is deselected, it will find the index of the FormControl with that value
 
-      // removing that FormControl from the FormArray
-      listBoxArray.removeAt(index);
+      listBoxArray.removeAt(index);    // removing that FormControl from the FormArray
     }
   }
 
-  // Handles the file input change event in my form
-  onFileChange(event: any, controlName: string) {
-    // getting the first file from the file input (since I’m only allowing single file uploads)
-    const file = event.target.files[0];
+ 
+  onFileChange(event: any, controlName: string) {  // Handles the file input change event in my form
 
-    // updating the form control with the selected file using patchValue
-    this.insertForm.patchValue({
+    const file = event.target.files[0];  // getting the first file from the file input (since I’m only allowing single file uploads)
+   
+    this.insertForm.patchValue({     // updating the form control with the selected file using patchValue
       [controlName]: file
     });
 
-    //  to update the validity of the form control after setting the value
-    this.insertForm.get(controlName)?.updateValueAndValidity();
+    
+    this.insertForm.get(controlName)?.updateValueAndValidity(); //  to update the validity of the form control after setting the value
   }
 
 
@@ -137,7 +134,6 @@ export class FormComponent {
 
 
       try {
-        // const response = await firstValueFrom(this.http.post('ttp://localhost:4242/api/insert-data-test', formData, { headers }));
         const response = await firstValueFrom(this.http.post('http://localhost:4242/api/insert-data-test', formData,{headers}));
 
         console.log('Data inserted successfully:', response);
@@ -149,36 +145,36 @@ export class FormComponent {
     }
   }
 
+}
 
 
+//   // Marked the method as 'async'
+//   async onUpdate() {
+//     if (this.insertForm.valid) {
+//       const formData = new FormData();
 
-  // Marked the method as 'async'
-  async onUpdate() {
-    if (this.insertForm.valid) {
-      const formData = new FormData();
 
-
-      for (const key in this.insertForm.controls) {
-        const controlValue = this.insertForm.get(key)?.value;
-        if (controlValue instanceof File) {
-          formData.append(key, controlValue);
-        } else {
-          formData.append(key, JSON.stringify(controlValue));
-        }
-      }
+//       for (const key in this.insertForm.controls) {
+//         const controlValue = this.insertForm.get(key)?.value;
+//         if (controlValue instanceof File) {
+//           formData.append(key, controlValue);
+//         } else {
+//           formData.append(key, JSON.stringify(controlValue));
+//         }
+//       }
    
 
-      const token = this.authService
-      const headers = new HttpHeaders({ 'Authorization': `Bearer ${token}` });
+//       const token = this.authService
+//       const headers = new HttpHeaders({ 'Authorization': `Bearer ${token}` });
 
-      try {
-        const response = await firstValueFrom(this.http.put('http://localhost:4242/api/update-data', formData));
-        console.log('Data updated successfully:', response);
-      } catch (error) {
-        console.error('Error updating data:', error);
-      }
-    } else {
-      console.error('Form is invalid');
-    }
-  }
-}
+//       try {
+//         const response = await firstValueFrom(this.http.put('http://localhost:4242/api/update-data', formData));
+//         console.log('Data updated successfully:', response);
+//       } catch (error) {
+//         console.error('Error updating data:', error);
+//       }
+//     } else {
+//       console.error('Form is invalid');
+//     }
+//   }
+// }
